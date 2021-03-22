98d8b56ad81a444e13b3750a1dbd8d62d3935193
GNUOverYooouu <69485115+DamieFC@users.noreply.github.com>
2021-03-22 10:57:37 -0700

    Add CLI

---
+++ cli/prompt.h
@@ -0,0 +1,8 @@
+/******************************************************************************
+ * CLI Functions                                                              *
+ * Copyleft DamieFC                                                           *
+ ******************************************************************************/
+#include <stdio.h>
+// Important information
+char prompt = "you@cozen: ";
+char input
